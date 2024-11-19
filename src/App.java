99124ae30241d42b481b0e223f843b05ba39dba6
Eduardo Rodrigues Rangel Malaquias <malaquias.eduardo18@gmail.com>
2024-11-18 22:38:13 -0300

    primeiro commit

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Funcionario funcionario = new Funcionario("Eduardo", true);
+
+        System.out.println("BEm vindo funcionario: "+ funcionario.nome);
+    }
+}
---
+++ src/Funcionario.java
@@ -0,0 +1,9 @@
+public class Funcionario{
+  public String nome;
+  public boolean apto;
+
+  public Funcionario(String nome, boolean apto) {
+      this.nome = nome;
+      this.apto = apto;
+  }
+} 
