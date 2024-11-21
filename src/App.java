a6d305acc8a98a867e0d20c84e4abc7d6a30b5d0
Eduardo Rodrigues Rangel Malaquias <malaquias.eduardo18@gmail.com>
2024-11-21 16:35:34 -0300

    logica de registro e leitura no csc

--- src/App.java
+++ src/App.java
@@ -1,12 +1,108 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        Funcionario funcionario = new Funcionario("Eduardo", false);
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
-        System.out.println("BEm vindo funcionario: "+ funcionario.nome);
-        if(funcionario.apto){
-            System.out.println("Voce é apto");
-        }else{
-            System.out.println("Voce é inapto");
+public class App {
+    public static void main(String[] args) {
+        Cargo ObjCargo =  new Cargo(4, "limpeza", 1300.00);
+        Funcionario funcionario = new Funcionario(4, "julia", "126.774.657-31", ObjCargo);
+        String nomeArquivo = "src/dados/Funcionarios.csv";
+
+        //adicionar aluno ao arquivo
+        try {
+            boolean arquivoExiste = new File(nomeArquivo).exists();
+            FileWriter escritor = new FileWriter(nomeArquivo, arquivoExiste);
+            if (!arquivoExiste) {
+                escritor.write("Id;Nome;Cpf;Cargo\n");
+            }
+
+            escritor.write(
+                funcionario.getId() + ";" + 
+                funcionario.getNome() + ";" +
+                funcionario.getCpf() + ";" +
+                funcionario.getCargo().getNome() + "\n");
+            escritor.flush();
+            escritor.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Deu ruin");
         }
+
+
+        try {
+            BufferedReader leitor = new BufferedReader(new FileReader(nomeArquivo));
+            String linha;
+            boolean primeiraLinha = true;
+
+            while ((linha = leitor.readLine()) != null ) { 
+                if (primeiraLinha) {
+                    primeiraLinha = false;
+                    continue;
+                }
+
+                String[] celula = linha.split(";");
+
+                int id = Integer.parseInt(celula[0]) ;
+                String nome = celula[1];
+                String cpf = celula[2];
+                String cargo = celula[3];
+            
+                System.out.println
+                ("Id: "+ id +
+                " - Nome: "+ nome +
+                " - Cpf: "+ cpf +
+                " - Cargo: "+ cargo);
+            }
+            leitor.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Deu ruin");
+        }
+
+
+        // consultar filme por id
+        int index = 3;
+        try {
+            BufferedReader leitor = new BufferedReader(new FileReader(nomeArquivo));
+            String linha;
+            boolean primeiraLinha = true;
+
+            while ((linha = leitor.readLine()) != null ) { 
+                if (primeiraLinha) {
+                    primeiraLinha = false;
+                    continue;
+                }
+
+                String[] celula = linha.split(";");
+
+                int id = Integer.parseInt(celula[0]) ;
+                
+
+                if(index == id){
+                    String nome = celula[1];
+                    String cpf = celula[2];
+                    String cargo = celula[3];
+                
+                    System.out.println
+                    ("Id: "+ id +
+                    " - Nome: "+ nome +
+                    " - Cpf: "+ cpf +
+                    " - Cargo: "+ cargo);
+                }
+
+                
+            }
+            leitor.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Deu ruin");
+        }
+        
     }
 }
+ 
--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,9 +1,50 @@
 public class Funcionario{
-  public String nome;
-  public boolean apto;
+  private int id;
+  private String nome;
+  private String cpf;
+  private Cargo cargo;
 
-  public Funcionario(String nome, boolean apto) {
-      this.nome = nome;
-      this.apto = apto;
-  }
+  public Funcionario(int id, String nome, String cpf, Cargo cargo) {
+    this.id = id;
+    this.nome = nome;
+    this.cpf = cpf;
+    this.cargo = cargo;
+}
+
+public int getId() {
+    return id;
+}
+
+public void setId(int id) {
+    this.id = id;
+}
+
+public String getNome() {
+    return nome;
+}
+
+public void setNome(String nome) {
+    this.nome = nome;
+}
+
+public String getCpf() {
+    return cpf;
+}
+
+public void setCpf(String cpf) {
+    this.cpf = cpf;
+}
+
+public Cargo getCargo() {
+    return cargo;
+}
+
+public void setCargo(Cargo cargo) {
+    this.cargo = cargo;
+} 
+
+@Override
+public String toString() {
+    return "ID: " + id + ", Nome: " + nome + ", CPF: " + cpf + ", Cargo: " + (cargo != null ? cargo.getNome() : "Nenhum");
+}
 } 
