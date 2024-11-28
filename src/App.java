69da07dc745111a1df03f5cae932a0721fb9effc
JulianoDADR <102887266+JulianoDADR@users.noreply.github.com>
2024-11-27 21:14:20 -0300

    Correção do EditarCargo

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -6,40 +8,46 @@ import java.util.Scanner;
 public class App {
     private static String arquivoFuncionarios = "src/dados/Funcionarios.csv";
     private static String arquivoCargos = "src/dados/Cargos.csv";
+    private static String arquivoPontos = "src/dados/RegistrosPontos.csv";
     private static List<Funcionario> funcionarios = new ArrayList<>();
     private static List<Cargo> cargos = new ArrayList<>();
+    private static List<RegistroPonto> registrosPontos = new ArrayList<>();
     private static int funcionarioId = 1;
     private static int cargoId = 1;
+    private static int registroPontosId = 1;
+    public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        
 
         carregarCargosDoArquivo();
         carregarFuncionariosDoArquivo();
+        carregarRegistroPontoDoArquivo();
 
         while (true) {
             System.out.println("\nMenu:");
-            System.out.println("1. Gerenciar Funcionários");
+            System.out.println("1. Gerenciar Funcionarios");
             System.out.println("2. Gerenciar Cargos");
-            System.out.println("3. Consultar Histórico de Funcionários");
+            System.out.println("3. Gerenciar Registros de Ponto");
             System.out.println("4. Sair");
-            System.out.print("Escolha uma opção: ");
+            System.out.print("Escolha uma opcao: ");
             int opcao = scanner.nextInt();
 
             switch (opcao) {
                 case 1:
-                    gerenciarFuncionarios(scanner);
+                    gerenciarFuncionarios();
                     break;
                 case 2:
-                    gerenciarCargos(scanner);
+                    gerenciarCargos();
                     break;
                 case 3:
-                    consultarFuncionarios();
+                    gerenciarRegistrosPontos();
                     break;
                 case 4:
                     System.out.println("Encerrando...");
                     salvarCargosNoArquivo();
                     salvarFuncionariosNoArquivo();
+                    salvarRegistrosNoArquivo();
                     scanner.close();
                     return;
                 default:
@@ -48,34 +56,34 @@ public class App {
         }
     }
 
-    private static void gerenciarFuncionarios(Scanner scanner) {
+    private static void gerenciarFuncionarios() {
         System.out.println("\nGerenciar Funcionários:");
         System.out.println("1. Cadastrar");
         System.out.println("2. Editar");
         System.out.println("3. Consultar");
         System.out.println("4. Apagar");
-        System.out.print("Escolha uma opção: ");
+        System.out.print("Escolha uma opcão: ");
         int opcao = scanner.nextInt();
 
         switch (opcao) {
             case 1:
-                cadastrarFuncionario(scanner);
+                cadastrarFuncionario();
                 break;
             case 2:
-                editarFuncionario(scanner);
+                editarFuncionario();
                 break;
             case 3:
                 consultarFuncionarios();
                 break;
             case 4:
-                apagarFuncionario(scanner);
+                apagarFuncionario();
                 break;
             default:
-                System.out.println("Opção inválida!");
+                System.out.println("Opcao invalida!");
         }
     }
 
-    private static void gerenciarCargos(Scanner scanner) {
+    private static void gerenciarCargos() {
         System.out.println("\nGerenciar Cargos:");
         System.out.println("1. Cadastrar");
         System.out.println("2. Editar");
@@ -87,10 +95,10 @@ public class App {
 
         switch (opcao) {
             case 1:
-                cadastrarCargo(scanner);
+                cadastrarCargo();
                 break;
             case 2:
-                editarCargo(scanner);
+                editarCargo();
                 break;
             case 3:
                 consultarCargos();
@@ -99,23 +107,51 @@ public class App {
                 consultarCargosComIds();
                 break;
             case 5:
-                apagarCargo(scanner);
+                apagarCargo();
                 break;
             default:
-                System.out.println("Opção inválida!");
+                System.out.println("Opcao invalida!");
         }
     }
 
+    private static void gerenciarRegistrosPontos() {
+      System.out.println("\n=== Gerenciar Registros de Pontos ===");
+      System.out.println("1. Registrar Ponto");
+      System.out.println("2. Editar Registro de Ponto");
+      System.out.println("3. Consultar Registros de Pontos");
+      System.out.println("4. Apagar Registro de Ponto");
+      System.out.print("Escolha uma opcao: ");
+
+      int opcao = scanner.nextInt();
+
+      switch (opcao) {
+          case 1:
+              registrarPonto();
+              break;
+          case 2:
+              editarRegistroPonto();
+              break;
+          case 3:
+              consultarRegistrosPontos();
+              break;
+          case 4:
+              apagarRegistroPonto();
+              break;
+          default:
+              System.out.println("Opcao invalida! Tente novamente.");
+      }
+  }
+
     // Métodos para Funcionários
-    private static void cadastrarFuncionario(Scanner scanner) {
+    private static void cadastrarFuncionario() {
         scanner.nextLine(); // Limpa o buffer após o último nextInt()
-        System.out.print("Nome do funcionário: ");
+        System.out.print("Nome do funcionario: ");
         String nome = scanner.nextLine(); // Captura o nome completo
-        System.out.print("CPF do funcionário: ");
+        System.out.print("CPF do funcionario: ");
         String cpf = scanner.next();
     
         if (cpfExiste(cpf)) {
-            System.out.println("Erro: CPF já cadastrado!");
+            System.out.println("Erro: CPF ja cadastrado!");
             return;
         }
     
@@ -125,14 +161,14 @@ public class App {
         Cargo cargo = buscarCargoPorId(idCargo);
         if (cargo != null) {
             funcionarios.add(new Funcionario(funcionarioId++, nome, cpf, cargo));
-            System.out.println("Funcionário cadastrado!");
+            System.out.println("Funcionario cadastrado!");
         } else {
             System.out.println("Cargo não encontrado!");
         }
     }
     
 
-    private static void editarFuncionario(Scanner scanner) {
+    private static void editarFuncionario() {
         System.out.print("ID do funcionário: ");
         int id = scanner.nextInt();
         Funcionario func = buscarFuncionarioPorId(id);
@@ -172,7 +208,7 @@ public class App {
         funcionarios.forEach(f -> System.out.println(f.getId() + " - " + f.getNome() + " - " + f.getCpf() + " - " + f.getCargo().getNome()));
     }
 
-    private static void apagarFuncionario(Scanner scanner) {
+    private static void apagarFuncionario() {
         System.out.print("ID do funcionário: ");
         int idApagar = scanner.nextInt();
         funcionarios.removeIf(f -> f.getId() == idApagar);
@@ -180,29 +216,47 @@ public class App {
     }
 
     // Métodos para Cargos
-    private static void cadastrarCargo(Scanner scanner) {
+    private static void cadastrarCargo() {
+        scanner.nextLine();
         System.out.print("Nome do cargo: ");
-        String nome = scanner.next();
+        String nome = scanner.nextLine();
+        System.out.print("Descrição do cargo: ");
+        String descricao = scanner.nextLine();
         System.out.print("Salário do cargo: ");
         double salario = scanner.nextDouble();
-        cargos.add(new Cargo(cargoId++, nome, salario));
+        cargos.add(new Cargo(cargoId++, nome, descricao, salario));
         System.out.println("Cargo cadastrado!");
     }
 
-    private static void editarCargo(Scanner scanner) {
+    private static void editarCargo() {
         System.out.print("ID do cargo: ");
         int id = scanner.nextInt();
+        scanner.nextLine(); // Consumir a quebra de linha restante
+    
         Cargo cargo = buscarCargoPorId(id);
         if (cargo != null) {
             System.out.print("Novo nome: ");
-            cargo.setNome(scanner.next());
+            cargo.setNome(scanner.nextLine()); // Permitir entrada com espaços
+    
+            System.out.print("Nova descricao: ");
+            cargo.setDescricao(scanner.nextLine()); // Permitir entrada com espaços
+    
             System.out.print("Novo salário: ");
-            cargo.setSalario(scanner.nextDouble());
+            if (scanner.hasNextDouble()) {
+                cargo.setSalario(scanner.nextDouble());
+                scanner.nextLine(); // Consumir a quebra de linha restante
+            } else {
+                System.out.println("Entrada inválida para salário.");
+                scanner.nextLine(); // Consumir entrada inválida
+                return;
+            }
+    
             System.out.println("Cargo atualizado!");
         } else {
             System.out.println("Cargo não encontrado!");
         }
     }
+    
 
     private static void consultarCargos() {
         if (cargos.isEmpty()) {
@@ -222,12 +276,78 @@ public class App {
         cargos.forEach(c -> System.out.println(c.getId() + " - " + c.getNome() + " - R$ " + c.getSalario()));
     }
 
-    private static void apagarCargo(Scanner scanner) {
+    private static void apagarCargo() {
         System.out.print("ID do cargo: ");
         int idApagar = scanner.nextInt();
         cargos.removeIf(c -> c.getId() == idApagar);
         System.out.println("Cargo removido!");
     }
+
+
+    //Metodos para Registro de Ponto
+    private static void registrarPonto() {
+      scanner.nextLine(); // Limpa o buffer após o último nextInt()
+      LocalDateTime agora = LocalDateTime.now();
+      DateTimeFormatter formatoData = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+      DateTimeFormatter formatoHora = DateTimeFormatter.ofPattern("HH:mm:ss");
+      System.out.println("Ponto sera registrado em: " + agora.format(formatoData) + "as " + agora.format(formatoHora));
+      consultarFuncionarios();
+      System.out.print("ID do funcionário: ");
+      int idFuncionario = scanner.nextInt(); // Captura o nome completo
+      Funcionario funcionario = buscarFuncionarioPorId(idFuncionario);
+  
+      System.out.println("Categorias: ");
+      System.out.println("[S] - Saida \n [E] - Entrada");
+      char categoria = scanner.next().toLowerCase().charAt(0);
+      if (funcionario != null) {
+          registrosPontos.add(new RegistroPonto(registroPontosId++, funcionario, categoria));
+          System.out.println("Resgistro cadastrado!");
+      } else {
+          System.out.println("Funcionario não encontrado!");
+      }
+    }
+
+    private static void editarRegistroPonto() {
+      System.out.print("ID do resgistro: ");
+      int id = scanner.nextInt();
+      RegistroPonto ponto = buscarRegistroPontoPorId(id);
+      if (ponto != null) {
+        consultarFuncionarios();
+        System.out.print("ID do novo funcionário: ");
+        int idFuncionario = scanner.nextInt(); // Captura o nome completo
+        Funcionario funcionario = buscarFuncionarioPorId(idFuncionario);
+        ponto.setFuncionario(funcionario);
+
+        System.out.println("Categorias: ");
+        System.out.println("[S] - Saida \n [E] - Entrada");
+        char categoria = scanner.next().toLowerCase().charAt(0);
+          if (funcionario != null) {
+            ponto.setCategoria(categoria);
+            System.out.println("Registro atualizado!");
+          } else {
+            System.out.println("Cargo não encontrado!");
+          }
+      } else {
+          System.out.println("Registro não encontrado!");
+      }
+  }
+
+  private static void consultarRegistrosPontos() {
+    if (registrosPontos.isEmpty()) {
+        System.out.println("Nenhum registro cadastrado.");
+        return;
+    }
+    System.out.println("\nLista de Registros:");
+    registrosPontos.forEach(r -> System.out.println(r.getId() + " - " + r.getFuncionario().getNome() + " - " + r.getData() + " - " + r.getHorario() + " - " + (r.getCategoria() == 'e' ? "Entrada" : "Saída")));
+}
+
+  private static void apagarRegistroPonto() {
+    System.out.print("ID do registro: ");
+    int idApagar = scanner.nextInt();
+    registrosPontos.removeIf(r -> r.getId() == idApagar);
+    System.out.println("Registro de ponto removido!");
+  }
+
 
     // Métodos auxiliares
     private static Funcionario buscarFuncionarioPorId(int id) {
@@ -238,6 +358,10 @@ public class App {
         return cargos.stream().filter(c -> c.getId() == id).findFirst().orElse(null);
     }
 
+    private static RegistroPonto buscarRegistroPontoPorId(int id) {
+      return registrosPontos.stream().filter(r -> r.getId() == id).findFirst().orElse(null);
+  }
+
     private static boolean cpfExiste(String cpf) {
         return funcionarios.stream().anyMatch(f -> f.getCpf().equals(cpf));
     }
@@ -245,13 +369,19 @@ public class App {
     private static void carregarFuncionariosDoArquivo() {
         try (BufferedReader reader = new BufferedReader(new FileReader(arquivoFuncionarios))) {
             String linha = reader.readLine(); // Pula o cabeçalho
+            
             while ((linha = reader.readLine()) != null) {
                 String[] dados = linha.split(";");
                 int id = Integer.parseInt(dados[0]);
+                
                 String nome = dados[1];
+              
                 String cpf = dados[2];
+               
                 int idCargo = Integer.parseInt(dados[3].trim());
+                
                 Cargo cargo = buscarCargoPorId(idCargo);
+                
                 if (cargo != null) {
                     funcionarios.add(new Funcionario(id, nome, cpf, cargo));
                     funcionarioId = Math.max(funcionarioId, id + 1);
@@ -269,14 +399,34 @@ public class App {
                 String[] dados = linha.split(";");
                 int id = Integer.parseInt(dados[0]);
                 String nome = dados[1];
-                double salario = Double.parseDouble(dados[2].trim());
-                cargos.add(new Cargo(id, nome, salario));
+                String descricao = dados[2];
+                double salario = Double.parseDouble(dados[3].trim());
+                cargos.add(new Cargo(id, nome, descricao, salario));
                 cargoId = Math.max(cargoId, id + 1);
             }
         } catch (IOException | NumberFormatException e) {
             System.out.println("Erro ao carregar cargos: " + e.getMessage());
         }
     }
+
+    private static void carregarRegistroPontoDoArquivo() {
+      try (BufferedReader reader = new BufferedReader(new FileReader(arquivoPontos))) {
+          String linha = reader.readLine(); // Pula o cabeçalho
+          while ((linha = reader.readLine()) != null) {
+              String[] dados = linha.split(";");
+              int id = Integer.parseInt(dados[0]);
+              int idFuncionario = Integer.parseInt(dados[1].trim());
+              Funcionario funcionario = buscarFuncionarioPorId(idFuncionario);
+              String data = dados[2];
+              String horario = dados[3];
+              char categoria = dados[4].charAt(0);
+              registrosPontos.add(new RegistroPonto(id, funcionario, data, horario, categoria));
+              registroPontosId = Math.max(cargoId, id + 1);
+          }
+      } catch (IOException | NumberFormatException e) {
+          System.out.println("Erro ao carregar Registros: " + e.getMessage());
+      }
+  }
 
     private static void salvarFuncionariosNoArquivo() {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(arquivoFuncionarios))) {
@@ -291,12 +441,24 @@ public class App {
 
     private static void salvarCargosNoArquivo() {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(arquivoCargos))) {
-            writer.write("Id;Nome;Salario\n");
+            writer.write("Id;Nome;Descricao;Salario\n");
             for (Cargo c : cargos) {
-                writer.write(c.getId() + ";" + c.getNome() + ";" + c.getSalario() + "\n");
+                writer.write(c.getId() + ";" + c.getNome() + ";" + c.getDescricao() + ";" + c.getSalario() + "\n");
             }
         } catch (IOException e) {
             System.out.println("Erro ao salvar cargos: " + e.getMessage());
         }
     }
+
+    private static void salvarRegistrosNoArquivo() {
+      try (BufferedWriter writer = new BufferedWriter(new FileWriter(arquivoPontos))) {
+          writer.write("Id;Funcionario;Data;Horario;Categoria\n");
+          for (RegistroPonto r : registrosPontos) {
+              writer.write(r.getId() + ";" + r.getFuncionario().getId() + ";" + r.getData() + ";" + r.getHorario() + ";" + r.getCategoria() + "\n");
+          }
+      } catch (IOException e) {
+          System.out.println("Erro ao salvar Registros de ponto: " + e.getMessage());
+      }
+  }
+    
 }
--- src/Cargo.java
+++ src/Cargo.java
@@ -1,56 +1,50 @@
 public class Cargo {
-  private int id;
-  private String nome;
-  private String descricao;
-  private double salario;
-
-  public Cargo(int id, String nome, double salario) {
-      this.id = id;
-      this.nome = nome;
-      this.salario = salario;
-  }
-
-  public Cargo(int id, String nome, String descricao, double salario) {
-      this.id = id;
-      this.nome = nome;
+    private int id;
+    private String nome;
+    private String descricao;
+    private double salario;
+  
+    public Cargo(int id, String nome, String descricao, double salario) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.salario = salario;
+    }
+  
+    public int getId() {
+        return id;
+    }
+  
+    public void setId(int id) {
+        this.id = id;
+    }
+  
+    public String getNome() {
+        return nome;
+    }
+  
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+  
+    public String getDescricao() {
+      return descricao;
+    }
+  
+    public void setDescricao(String descricao) {
       this.descricao = descricao;
-      this.salario = salario;
+    }
+  
+    public double getSalario() {
+        return salario;
+    }
+  
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+  
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Nome: " + nome + ", Descricao: " + descricao + ", Salário: R$" + salario;
+    }
   }
-
-  public int getId() {
-      return id;
-  }
-
-  public void setId(int id) {
-      this.id = id;
-  }
-
-  public String getNome() {
-      return nome;
-  }
-
-  public void setNome(String nome) {
-      this.nome = nome;
-  }
-
-  public String getDescricao() {
-    return descricao;
-  }
-
-  public void setDescricao(String descricao) {
-    this.descricao = descricao;
-  }
-
-  public double getSalario() {
-      return salario;
-  }
-
-  public void setSalario(double salario) {
-      this.salario = salario;
-  }
-
-  @Override
-  public String toString() {
-      return "ID: " + id + ", Nome: " + nome + ", Descricao: " + descricao + ", Salário: R$" + salario;
-  }
-}
